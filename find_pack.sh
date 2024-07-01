e04447d70ae61c496d252c35cc0ba973ef911dd5
fgwsz <3191000458@qq.com>
2024-07-02 00:28:19 +0800

    clean

--- find_pack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-read -p "Input Card ID: " card_id
-regex_pattern="[0-9]{5,8}"
-if [[ $card_id =~ $regex_pattern ]]; then
-    grep "^$card_id$" ../pack/* 2>/dev/null || echo "Card ID Not Found!"
-else
-    echo "Card ID Error!"
-fi
