13bde5f2e37f68dfdeaeab301a9a4a60150f689a
fgwsz <3191000458@qq.com>
2024-10-23 23:17:55 +0800

    clean push-deck

--- push-deck.ps1
+++ push-deck.ps1
@@ -1,5 +1,8 @@
 $root_path=Split-Path -Parent $MyInvocation.MyCommand.Definition
 cd "$root_path"
+git status
+git add -A
+git commit -m "delete deck"
 git add ocg/*.ydk
 git add 408/*.ydk
 git commit -m "push deck"
--- push-deck.sh
+++ push-deck.sh
@@ -2,6 +2,9 @@
 
 root_path=$(dirname "$(readlink -f "$0")")
 cd "$root_path"
+git status
+git add -A
+git commit -m "delete deck"
 git add ocg/*.ydk
 git add 408/*.ydk
 git commit -m "push deck"
