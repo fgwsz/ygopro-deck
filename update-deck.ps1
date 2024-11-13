aa85e0079d91e45b8fc27064a6e45d8e0744f8e8
fgwsz <3191000458@qq.com>
2024-11-13 21:24:26 +0800

    rename push deck to update deck

--- update-deck.ps1
+++ update-deck.ps1
@@ -2,9 +2,8 @@ $root_path=Split-Path -Parent $MyInvocation.MyCommand.Definition
 cd "$root_path"
 git status
 git add -A
-git commit -m "delete deck"
 git add ocg/*.ydk
 git add 408/*.ydk
 git add 2011_11_11/*.ydk
-git commit -m "push deck"
+git commit -m "Update deck."
 git push
