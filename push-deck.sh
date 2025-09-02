af55fc0a6fc2cb4179fd5e63ad8bc83c5ee70102
fgwsz <3191000458@qq.com>
2025-09-02 11:19:46 +0800

    fix sh if []; then

--- push-deck.sh
+++ push-deck.sh
@@ -3,7 +3,7 @@
 root_path=$(dirname "$(readlink -f "$0")")
 cd "$root_path"
 
-if git status --porcelain | grep -E '^(.. )?((ocg|408|2011_11_11)/.*\.ydk$|(ocg|408|2011_11_11)/.*\.ydk$)' >/dev/null;
+if [ git status --porcelain | grep -E '^(.. )?((ocg|408|2011_11_11)/.*\.ydk$|(ocg|408|2011_11_11)/.*\.ydk$)' >/dev/null ]; then
     git add -A ocg/*.ydk 408/*.ydk 2011_11_11/*.ydk
     echo "卡组未变更"
     git commit -m "自动提交卡组变更"
