6019221a475a642fa3a78da82750b7f4e0a50287
fgwsz <3191000458@qq.com>
2025-09-02 11:35:33 +0800

    rewrite push-deck.ps1

--- push-deck.ps1
+++ push-deck.ps1
@@ -1,20 +1,14 @@
-#!/usr/bin/env pwsh
-
-# 获取脚本所在目录的绝对路径
 $root_path = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Set-Location $root_path
 
-# 检查变更（包括未暂存、未跟踪的文件）
-$stagedChanges = git diff --quiet --cached
-$unstagedChanges = git diff --quiet
+git add -A
 
-if ($LASTEXITCODE -eq 0 -and $unstagedChanges -eq 0) {
+$hasChanges = git status --porcelain | Select-String -Pattern '^(.. )?((ocg|408|2011_11_11)/.*\.ydk$)'
+if ($hasChanges) {
+    git status
+    git add ocg/*.ydk, 408/*.ydk, 2011_11_11/*.ydk
+    git commit -m "自动提交卡组变更"
+    git push
+} else {
     Write-Output "卡组未变更"
-    exit 0
 }
-
-# 提交并推送
-git status
-git add -A ocg/*.ydk, 408/*.ydk, 2011_11_11/*.ydk
-git commit -m "自动提交卡组变更"
-git push
