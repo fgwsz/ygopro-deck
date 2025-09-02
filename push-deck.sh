11115615a329542e78081ec0932c1c8ddd093d4f
fgwsz <3191000458@qq.com>
2025-09-02 11:25:24 +0800

    fix echo bug

--- push-deck.sh
+++ push-deck.sh
@@ -3,9 +3,12 @@
 root_path=$(dirname "$(readlink -f "$0")")
 cd "$root_path"
 
+git add -A
 if git status --porcelain | grep -E '^(.. )?((ocg|408|2011_11_11)/.*\.ydk$|(ocg|408|2011_11_11)/.*\.ydk$)' >/dev/null; then
-    git add -A ocg/*.ydk 408/*.ydk 2011_11_11/*.ydk
-    echo "卡组未变更"
+    git status
+    git add ocg/*.ydk 408/*.ydk 2011_11_11/*.ydk
     git commit -m "自动提交卡组变更"
     git push
+else
+    echo "卡组未变更"
 fi
