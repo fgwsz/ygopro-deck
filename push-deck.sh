ccf219655b5b322e3579d385d6473bdebf2670eb
fgwsz <3191000458@qq.com>
2025-09-02 11:13:39 +0800

    fix 卡组删除无法同步问题

--- push-deck.sh
+++ push-deck.sh
@@ -3,14 +3,10 @@
 root_path=$(dirname "$(readlink -f "$0")")
 cd "$root_path"
 
-# 检查变更（包括未暂存、未跟踪的文件）
-if git diff --quiet --cached && git diff --quiet; then
+git add -A ocg/*.ydk 408/*.ydk 2011_11_11/*.ydk
+if [ -z "$(git status --porcelain)" ]; then
     echo "卡组未变更"
     exit 0
 fi
-
-# 提交并推送
-git status
-git add -A ocg/*.ydk 408/*.ydk 2011_11_11/*.ydk
 git commit -m "自动提交卡组变更"
 git push
